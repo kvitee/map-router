7971f433f43e47d796c5443610079f171bc5de32
kvitee <kvitee41@gmail.com>
2024-06-12 16:05:24 +0800

    Create Var_str module which contains Stack-related structures and functions.

---
+++ include/Var_str.h
@@ -0,0 +1,63 @@
+#ifndef VAR_STR_H_INCLUDED_
+#define VAR_STR_H_INCLUDED_
+
+#include <stdint.h>
+
+
+/* Тип данных, хранящихся в стеке. */
+typedef int Stack_data_t;
+
+/* Узел стека */
+typedef struct stack_node {
+  /* Указатель на предыдущий узел. */
+  struct stack_node *prev;
+
+  /* Хранящиеся данные. */
+  Stack_data_t data;
+} Stack_Node;
+
+/* Стек */
+typedef struct stack {
+  /* Указатель на узел, являющийся вершиной стека. */
+  Stack_Node *top;
+} Stack;
+
+
+/**
+ * Создает пустой стек и возвращает указатель на него.
+ */
+Stack *Stack__create(void);
+
+/**
+ * Проверяет, является ли стек пустым.
+ * Возвращает 1, если стек не сожержит данных, иначе 0.
+ */
+uint8_t Stack__empty(const Stack *s);
+
+/**
+ * Возвращает данные, хранящиеся на вершине стека.
+ */
+Stack_data_t Stack__top(Stack *s);
+
+/**
+ * Добавляет данные на вершину стека.
+ */
+void Stack__push(Stack *s, Stack_data_t data);
+
+/**
+ * Удаляет данные с вершины стека.
+ */
+void Stack__pop(Stack *s);
+
+/**
+ * Удаляет все данные из стека.
+ */
+void Stack__clear(Stack *s);
+
+/**
+ * Удаляет все данные стека и освобождает выделенную под него память.
+ */
+void Stack__free(Stack *s);
+
+
+#endif  /* VAR_STR_H_INCLUDED_ */
---
+++ src/Var_str.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "Var_str.h"
+
+
+Stack *Stack__create(void) {
+  Stack *s = malloc(sizeof(Stack));
+
+  s->top = malloc(sizeof(Stack_Node));
+  s->top->prev = NULL;
+
+  return s;
+}
+
+uint8_t Stack__empty(const Stack *s) {
+  return (s == NULL) || (s->top->prev == NULL);
+}
+
+Stack_data_t Stack__top(Stack *s) {
+  if (s == NULL) {
+    return (Stack_data_t){0};
+  }
+
+  return s->top->data;
+}
+
+void Stack__push(Stack *s, Stack_data_t data) {
+  if (s == NULL) {
+    return;
+  }
+
+  Stack_Node *n = malloc(sizeof(Stack_Node));
+
+  n->prev = s->top;
+  n->data = data;
+
+  s->top = n;
+}
+
+void Stack__pop(Stack *s) {
+  if (Stack__empty(s)) {
+    return;
+  }
+
+  Stack_Node *top = s->top;
+
+  s->top = s->top->prev;
+  free(top);
+}
+
+void Stack__clear(Stack *s) {
+  while (!Stack__empty(s)) {
+    Stack__pop(s);
+  }
+}
+
+void Stack__free(Stack *s) {
+  if (s == NULL) {
+    return;
+  }
+
+  Stack__clear(s);
+
+  free(s->top);
+  free(s);
+}
