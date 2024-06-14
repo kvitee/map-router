3385f0ce5ec26d2250b3395c35b277da1f20638c
kvitee <kvitee41@gmail.com>
2024-06-14 13:49:42 +0800

    Add more comments to Alg.c module, refactor some blocks.

--- src/Alg.c
+++ src/Alg.c
@@ -10,31 +10,31 @@
 /**
  * Вычисляет расстояние между двумя точками маршрута.
  */
-float dist(const Waypoint p1, const Waypoint p2) {
+float dist(Waypoint p1, Waypoint p2) {
   return hypotf(p1.x - p2.x, p1.y - p2.y);
 }
 
 /**
- * Ищет первую секцию карты, содержащую символ sym.
+ * Ищет первую ячейку карты, содержащую символ sym.
  * Возвращает соответствующую ей точку маршрута.
  */
 Waypoint find_symbol(const Map *m, Map_symbol_code s) {
   for (uint8_t i = 0; i < m->h; i++) {
     for (uint8_t j = 0; j < m->w; j++) {
       if (Map__get(m, j, i) == s) {
-        return (Waypoint){j, i, 0, 0};
+        return (Waypoint){j, i, 0, 0, 0.0f};
       }
     }
   }
 
-  return (Waypoint){-1, -1, 0, 0};
+  return (Waypoint){-1, -1, 0, 0, 0.0f};
 }
 
 /**
- * Проверяет, является ли секция посещенной на основе переданного
- * стека посещенных секций.
+ * Проверяет, является ли ячейка посещенной на основе переданного
+ * стека посещенных ячеек.
  *
- * Возвращает 1 если секция посещена (находится в visited), иначе 0.
+ * Возвращает 1 если ячейка посещена (находится в visited), иначе 0.
  */
 uint8_t is_visited(Waypoint w, const Stack *visited) {
   Stack_Node *cn = visited->top;
@@ -49,9 +49,13 @@ uint8_t is_visited(Waypoint w, const Stack *visited) {
   return 0;
 }
 
+/**
+ * Сортирует массив точек маршрута arr длины len в порядке убывания
+ * их расстояний от конца маршрута (величины h).
+ */
 void sort_waypoints(Waypoint *arr, uint8_t len) {
-  for (int i = 0; i < len; i++) {
-    for (int j = 0; j < len; j++) {
+  for (uint8_t i = 0; i < len; i++) {
+    for (uint8_t j = 0; j < len; j++) {
       if (arr[i].h > arr[j].h) {
         Waypoint tmp = arr[i];
         arr[i] = arr[j];
@@ -61,33 +65,24 @@ void sort_waypoints(Waypoint *arr, uint8_t len) {
   }
 }
 
-Stack *get_route(Waypoint start, Waypoint end, Stack *visited) {
-  Stack *route = Stack__create();
+/**
+ * Проверяет, возможно ли передвинуться в точку w и ее родительской
+ * на карте m.
+ * Возвращает 1, если возможно, иначе 0.
+ */
+uint8_t move_blocked(Waypoint w, const Map *m) {
+  if (Map__get(m, w.x + w.dx, w.y) != FREE && Map__get(m, w.x, w.y + w.dy) != FREE) return 1;
 
-  Stack__push(route, end);
-  Waypoint p = Waypoint__parent(end);
-  while (!Stack__empty(visited)) {
-    if (!Waypoint__equal(p, Stack__top(visited))) {
-      Stack__pop(visited);
-      continue;
-    }
+  if (Map__get(m, w.x, w.y) == BORDER) return 1;
+  if (Map__get(m, w.x, w.y) == OBSTACLE) return 1;
 
-    Stack__push(route, Stack__top(visited));
-    Stack__pop(visited);
-
-    if (Waypoint__equal(Stack__top(route), start)) break;
-
-    p = Waypoint__parent(Stack__top(route));
-  }
-
-  return route;
+  return 0;
 }
-
 
 /**
  * Основная функция для поиска маршрута на карте.
- * Не изменяет саму карту, а возвращает стек точек маршрута.
- * 
+ * Не изменяет саму карту.
+ * Возвращает стек точек маршрута, если таковой найден, иначе NULL.
  */
 Stack *find_route(const Map *m) {
   /* Актуальный стек - стек с секциями, которые предстоит
@@ -126,26 +121,37 @@ Stack *find_route(const Map *m) {
     Waypoint neighbors[8];
     uint8_t nc = 0;
 
+    static int8_t directions[8][2] = {
+      {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}
+    };
+
+    /* Перебираем соседние ячейки. */
     Waypoint n;
     for (uint8_t i = 0; i < 8; i++) {
-      n.dx = ((0x901a >> i*2) & 3) - 1;
-      n.dy = ((0xa901 >> i*2) & 3) - 1;
+      n.dx = directions[i][0];
+      n.dy = directions[i][1];
 
       n.x = c.x - n.dx;
       n.y = c.y - n.dy;
 
-      if (n.dx != 0 && n.dy != 0 && Map__get(m, n.x + n.dx, n.y) != FREE && Map__get(m, n.x, n.y + n.dy) != FREE) continue;
+      /* Если в выбранном направлении невозможно переместиться,
+       * пропускаем ячейку. */
+      if (move_blocked(n, m)) continue;
 
-      if (Map__get(m, n.x, n.y) == BORDER) continue;
-      if (Map__get(m, n.x, n.y) == OBSTACLE) continue;
-
+      /* Если ячейка уже посещена, также пропускаем ее. */
       if (is_visited(n, visited)) continue;
 
+      /* Вычисляем расстояние до конца маршрута. */
       n.h = dist(n, end);
 
+      /* Сохраняем в массив соседей. */
       neighbors[nc++] = n;
     }
 
+    /* Сортируем соседей по убыванию их расстояния до конца маршрута,
+     * чтобы после добавления их в стек actual на вершине находилась
+     * ячейка с наименьшим расстоянием до конца маршрута.
+     */
     sort_waypoints(neighbors, nc);
 
     for (uint8_t i = 0; i < nc; i++) {
@@ -153,25 +159,49 @@ Stack *find_route(const Map *m) {
     }
   }
 
-  Stack *route = get_route(start, end, visited);
+  /* Если после завершения основного цикла на вершине стека с посещенными
+   * ячейками находится не конец маршрута, то маршрут не найден, и
+   * функция возвращает NULL - пустой маршрут.
+   */
+  if (!Waypoint__equal(Stack__top(visited), end)) {
+    return NULL;
+  }
 
+  /* Создаем финальный стек, содержащий */
+  Stack *route = Stack__create();
+
+  /* Добавляем конец маршрута и вычисляем предыдущую точку маршрута (p). */
+  Stack__push(route, end);
+  Waypoint p = Waypoint__parent(end);
+
+  /* Основной цикл, работает, пока есть посещенные вершины.
+   * Выполняет поиск предыдущей точки маршрута в стеке посещенных,
+   * Когда находит, добавляет ее в стек маршрута и обновляет p,
+   * чтобы на следующих итерациях искалась уже ее предыдущая точка.
+   */
+  while (!Stack__empty(visited)) {
+    /* Если точка на вершине стека не является искомой, пропускаем ее. */
+    if (!Waypoint__equal(p, Stack__top(visited))) {
+      Stack__pop(visited);
+      continue;
+    }
+
+    /* Добавляем найденную точку в стек маршрута. */
+    Stack__push(route, Stack__top(visited));
+    Stack__pop(visited);
+
+    /* Если найденная точка - начало маршрута, выходим из цикла. */
+    if (Waypoint__equal(Stack__top(route), start)) break;
+
+    /* Обновляем искомую точку. */
+    p = Waypoint__parent(Stack__top(route));
+  }
+
+  /* Очищаем все вспомогательные стеки. */
   Stack__free(actual);
   Stack__free(visited);
 
   return route;
-}
-
-void mark_route(Map *m, Stack *r) {
-  Waypoint c;
-  while (!Stack__empty(r)) {
-    c = Stack__top(r);
-
-    if (Map__get(m, c.x, c.y) == FREE) {
-      Map__set(m, c.x, c.y, ROUTE);
-    }
-
-    Stack__pop(r);
-  }
 }
 
 
@@ -195,10 +225,27 @@ int main(int argc, char **argv) {
   Map *map = Map__read(f);
   fclose(f);
 
+  /* Выполняем поиск маршрута. */
   Stack *route = find_route(map);
 
-  mark_route(map, route);
+  /* Если маршрут не найден, выводим сообщение об этом. */
+  if (route == NULL) {
+    printf("Маршрут не найден.\n");
+  }
 
+  /* Отмечаем найденный маршрут на карте. */
+  Waypoint c;
+  while (!Stack__empty(route)) {
+    c = Stack__top(route);
+
+    if (Map__get(map, c.x, c.y) == FREE) {
+      Map__set(map, c.x, c.y, ROUTE);
+    }
+
+    Stack__pop(route);
+  }
+
+  /* Выводим карту с найденным маршрутом в консоль. */
   print_map(map, stdout);
 
   Stack__free(route);
